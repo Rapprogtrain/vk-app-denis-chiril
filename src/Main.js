035c898f8adf6d40714b65adcbf93f7ad3033848
Rapprogtrain <rapprogtraindenisite@gmail.com>
2020-05-27 15:01:58 +0300

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Div, Group, Panel, Text, View} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <View>
+            <Text>Новости</Text>
+        </View>
+        );
+    }
+}
+
+export default Main;
